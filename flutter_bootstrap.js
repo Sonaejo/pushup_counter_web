ebdd694514b6d48f97959b772c73453015affe95
Sonaejo <spr2440064@stu.o-hara.ac.jp>
2025-11-18 13:50:26 +0900

    Rebuild Flutter web with base-href

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1279480396"
+    serviceWorkerVersion: "3150759180"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -92,7 +92,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "6679f229f7f363940d14b296b12a9b9b
 "face_runtime.js": "bd3b5f922d658e8be4c3cf969cbf63f1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "2ecd134572347291819a63a921886088",
+"flutter_bootstrap.js": "555e0eada23b9193d31b807655868772",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
